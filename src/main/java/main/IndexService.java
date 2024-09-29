18aaba44df68d1b3a4df6c60c84950b91b9e2a00
jaeheoon <aleh101@naver.com>
2024-09-29 23:31:42 +0900

    관리자 페이지 누락 사항 커밋

--- src/main/java/main/IndexService.java
+++ src/main/java/main/IndexService.java
@@ -2,13 +2,23 @@ package main;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.control.CommandProcess;
+
+import member.bean.MemberDTO;
 
 public class IndexService implements CommandProcess {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		HttpSession session = request.getSession();
+		if(session.getAttribute("memberDTO") != null) {
+			MemberDTO memberDTO = (MemberDTO)session.getAttribute("memberDTO");
+			if (memberDTO.getAdmin() == 1) {
+				return "/admin/main.jsp";
+			}
+		}
 		return "/index.jsp";
 	}
 
